8b88efa9603b40577495def8ff5f5791f3ece63b
satvr <satvr@126.com>
2018-07-11 18:00:07 +0800

    first commit

---
+++ src/StringArrayTest.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class StringArrayTest {
+
+	public static void main(String[] args) {
+		String[] abc = { "a", "b", "c", "d", "e", "f", "g" };
+		System.out.println("length: " + abc.length);
+		System.out.println("Arrays.asList: " + Arrays.asList(abc));
+	}
+
+}
---
+++ src/StringToIngeter.java
@@ -0,0 +1,13 @@
+
+public class StringToIngeter {
+
+	@SuppressWarnings("unlikely-arg-type")
+	public static void main(String[] args) {
+		Integer int1 = 5;
+		String str1 = "4";
+		System.out.println("int1 is: " + int1);
+		System.out.println("str1 is: " + str1);
+		System.out.println(int1.equals(str1));
+	}
+
+}
