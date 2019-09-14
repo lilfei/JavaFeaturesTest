cbde6f104fa355fa959e1ee8c1c9fa6f45ad39fb
Satvr <satvr@126.com>
2019-09-14 23:22:19 +0800

    java spider

---
+++ src/com/llf/spider/test1/Test.java
@@ -0,0 +1,11 @@
+package com.llf.spider.test1;
+
+public class Test {
+
+	public static void main(String[] args) {
+		String url = "5ef5edbdc64c1bb49e9d6899";
+		String result = Analyze.regexMain(url);
+		System.out.println(result);
+	}
+
+}
