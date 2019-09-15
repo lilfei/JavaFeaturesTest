3467dd158214c3e762a0ee7d460d140f37c1de53
Satvr <satvr@126.com>
2019-09-15 21:14:41 +0800

    加入jsoup解析58房源信息

--- src/com/llf/spider/houma_58/AnalyzeHouse58.java
+++ src/com/llf/spider/houma_58/AnalyzeHouse58.java
@@ -10,6 +10,11 @@ import java.io.OutputStreamWriter;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 import com.llf.spider.test1.ConnectionUtil;
 
 public class AnalyzeHouse58 {
@@ -22,19 +27,51 @@ public class AnalyzeHouse58 {
 
 	private static String getHouseInfo(String result) {
 //		writeFile(result);
-		
+
 		String html = readFile();
-		
-//		System.out.println("htmlStr: " + htmlStr);
-		
-		// 提取房源list
-		Pattern listPattern = Pattern.compile("<ul class='house-list-wrap'>.*<!-- 分页 begin-->");
-		Matcher listMatcher = listPattern.matcher(html);
-		while (listMatcher.find()) {
-			String houseList = listMatcher.group();
+
+		// Jsoup解析html
+		Document doc = Jsoup.parse(html);
+
+		int i = 0;
+
+		// 获取list列表
+		Elements listEle = doc.getElementsByClass("house-list-wrap");
+		for (Element ele : listEle) {
+			// 获取每个单独数据
+			Elements listLi = ele.getElementsByTag("li");
+			for (Element e : listLi) {
+				// 解析数据内容
+				i++;
+				System.out.println("====================================================== " + i);
+//				System.out.println(e.text());
+				Elements pic = e.getElementsByClass("pic");
+				System.out.println("pic： " + pic.text());
+
+				Elements listInfos = e.getElementsByClass("list-info");
+//				System.out.println("listInfo: " + listInfos.text());
+				for (Element listInfo : listInfos) {
+					Elements title = listInfo.getElementsByClass("title");
+					System.out.println("标题: " + title.text());
+
+					Elements baseInfos = listInfo.getElementsByClass("baseinfo");
+					for (Element baseInfo : baseInfos) {
+						System.out.println("具体信息： " + baseInfo.text());
+					}
+
+					Elements jjrinfo = listInfo.getElementsByClass("jjrinfo");
+					System.out.println("房源： " + jjrinfo.text());
+				}
+
+				Elements price = e.getElementsByClass("price");
+				System.out.println("价格: " + price.text());
+
+				Elements time = e.getElementsByClass("time");
+				System.out.println("更新时间: " + time.text());
+			}
 		}
-		
-		return html;
+
+		return "";
 	}
 
 	private static String readFile() {
@@ -43,8 +80,8 @@ public class AnalyzeHouse58 {
 		try {
 			is = new FileInputStream("fileOut.html");
 			InputStreamReader reader = new InputStreamReader(is, "UTF-8");
-			while(reader.ready()) {
-				sb.append((char)reader.read());
+			while (reader.ready()) {
+				sb.append((char) reader.read());
 			}
 			reader.close();
 			is.close();
@@ -59,8 +96,8 @@ public class AnalyzeHouse58 {
 	private static void writeFile(String result) {
 		System.out.println(result);
 		File file = new File("fileOut.html");
-		byte bt[] = new byte[1024];
-		bt = result.getBytes();
+//		byte bt[] = new byte[1024];
+//		bt = result.getBytes();
 		try {
 			FileOutputStream os = new FileOutputStream(file);
 			OutputStreamWriter writer = new OutputStreamWriter(os, "UTF-8");
