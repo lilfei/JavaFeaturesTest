00b0db64d147882621da0cad49d78081a4143881
Satvr <satvr@126.com>
2020-11-08 11:30:16 +0800

    注释

--- src/com/llf/javaTest/classTest/UpCastingTest.java
+++ src/com/llf/javaTest/classTest/UpCastingTest.java
@@ -11,6 +11,12 @@ class Instrument {
 	}
 }
 
+/**
+ * 向上转型测试，导出类可以含有更多的方法
+ * 但它必须具备基类中所含有的方法
+ * @author llf
+ * @date 2020年11月8日
+ */
 public class UpCastingTest extends Instrument {
 
 	public static void main(String[] args) {
